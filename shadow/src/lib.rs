e5a249892d7168156c5bcca78b71fc2538e28e02
kavish <kavishwer2301@gmail.com>
2026-01-16 13:51:05 +0800

    Feat: Add Shadow Stack

---
+++ shadow/src/lib.rs
@@ -0,0 +1,60 @@
+#![no_std]
+
+const STACK_POINTER: u32 = 12;
+const FRAME_POINTER: u32 = 12;
+const STACK_POINTER_ADDR: u32 = 4;
+const FRAME_POINTER_ADDR: u32 = 8;
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+unsafe fn read_u32(addr: u32) -> u32 {
+    *(addr as *const u32)
+}
+
+unsafe fn write_u32(addr: u32, val: u32) {
+    *(addr as *mut u32) = val;
+}
+
+#[no_mangle]
+pub extern "C" fn init() {
+    unsafe {
+        write_u32(STACK_POINTER_ADDR, STACK_POINTER);
+        write_u32(FRAME_POINTER_ADDR, FRAME_POINTER);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn push(size: u32) {
+    unsafe {
+        let offset = size * 8 + 4;
+        let sp = read_u32(STACK_POINTER_ADDR);
+        let fp = read_u32(FRAME_POINTER_ADDR);
+
+        write_u32(sp + offset - 4, fp);
+        write_u32(FRAME_POINTER_ADDR, sp);
+        write_u32(STACK_POINTER_ADDR, sp + offset);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn pop() {
+    unsafe {
+        let sp = read_u32(STACK_POINTER_ADDR);
+        let fp = read_u32(FRAME_POINTER_ADDR);
+
+        write_u32(STACK_POINTER_ADDR, fp);
+        write_u32(FRAME_POINTER_ADDR, read_u32(sp - 4));
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn set(index: u32, ty: u32, value: u32) {
+    unsafe {
+        let fp = read_u32(FRAME_POINTER_ADDR);
+        write_u32(fp - (index * 8), ty);
+        write_u32(fp + (index * 8) + 4, value);
+    }
+}
